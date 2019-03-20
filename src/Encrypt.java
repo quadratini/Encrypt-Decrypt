4377a4f47a4f4a9795dc8aa33ce0c532bd5a943f
Yinghao Wang <yinghao.a.wang@gmail.com>
2019-03-20 01:58:10 -0700

    refactored code, fixed bugs with reading file

--- src/Encrypt.java
+++ src/Encrypt.java
@@ -10,6 +10,7 @@
 * @verison 03/17/2019
 *
 */
+import java.nio.file.Files;
 import java.util.Scanner;
 import java.io.File;
 import java.io.FileNotFoundException;
@@ -24,14 +25,18 @@ public class Encrypt {
         Scanner in = new Scanner(System.in);
         Scanner fileIn;
         PrintStream out;
-        String answer = "";
+        char answer = ' ';
 
         // Tries to find an input file
         while (true) {
             try {
                 System.out.println("File to encrypt/decrypt:");
                 String fileName = FILES_DIR + in.next();
-                fileIn = new Scanner(new File(fileName).getAbsolutePath());
+                File file = new File(fileName);
+                if (!file.exists()) {
+                    throw new FileNotFoundException();
+                }
+                fileIn = new Scanner(new File(fileName));
                 break;
             } catch (Exception e) {
                 System.out.println("Can't find file.");
@@ -50,40 +55,57 @@ public class Encrypt {
             }
         }
 
-        while (!answer.equalsIgnoreCase("encrypt") && !answer.equalsIgnoreCase("decrypt")) {
+        // Asks if user wants to encrypt or decrypt
+        while (answer != 'd' && answer != 'e') {
             System.out.println("Encrypt or decrypt?");
-            String ende = in.next();
-            in.nextLine(); // consumes the rest of the line if the input is invalid.
-            answer = ende;
+            String nextIn = in.next();
+            answer = nextIn.charAt(0);
         }
-        
-        String s = encryptFile(fileIn, answer);
+
+        String res = endecryptFile(fileIn, answer);
         
         System.setOut(out);
-        System.out.println(s);
+        System.out.println(res);
     }
     
-    public static String encryptFile(Scanner fileIn, String ende) {
+    public static String endecryptFile(Scanner fileIn, char answer) {
         String encrypted = "";
         while (fileIn.hasNextLine()) {
             String line = fileIn.nextLine();
-            String newWord = encrypt(line, ende);
+            String newWord;
+            if (answer == 'e') {
+                newWord = encrypt(line);
+            } else {
+                newWord = decrypt(line);
+            }
+
             encrypted += newWord;
         }
         return encrypted;
     }
-   
-    public static String encrypt(String line, String ende) {
+
+    public static String decrypt(String line) {
+        String decrypted = "";
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            if (c == ' ' || c == '\n' || c == '\t') {
+                decrypted += (char) c;    // CAST TO CHAR or else "lossy conversion"
+            } else {
+                decrypted += (char)(c - 1);
+            }
+        }
+        return decrypted + "\n";
+    }
+
+    public static String encrypt(String line) {
         String encrypted = "";
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
-            if (c == ' ' || c == '\n' || c == '\t') { 
-                encrypted += (char)c;    // CAST TO CHAR or else "lossy conversion"
-            } else if (ende.equalsIgnoreCase("encrypt")) {
+            if (c == ' ' || c == '\n' || c == '\t') {
+                encrypted += (char) c;    // CAST TO CHAR or else "lossy conversion"
+            } else {
                 encrypted += (char)(c + 1);
-            } else if (ende.equalsIgnoreCase("decrypt")){
-                encrypted += (char)(c - 1);
-            }             
+            }
         }
         return encrypted + "\n";
     }  
